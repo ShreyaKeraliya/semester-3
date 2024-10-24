088e84a7f084c73df34b6ba631ad9e8fe69c2932
ShreyaKeraliya <140882904+ShreyaKeraliya@users.noreply.github.com>
2024-10-24 22:47:57 +0530

    Update Prices.js

--- WebTechnology/Project/iscream/frontend/src/components/Prices.js
+++ WebTechnology/Project/iscream/frontend/src/components/Prices.js
@@ -1,32 +1,32 @@
 export const Prices = [
     {
       _id: 0,
-      name: "$0 to 19",
+      name: "10 to 29",
       array: [0, 19],
     },
     {
       _id: 1,
-      name: "$20 to 39",
+      name: "30 to 49",
       array: [20, 39],
     },
     {
       _id: 2,
-      name: "$40 to 59",
+      name: "50 to 69",
       array: [40, 59],
     },
     {
       _id: 3,
-      name: "$60 to 79",
+      name: "70 to ",89
       array: [60, 79],
     },
     {
       _id: 4,
-      name: "$80 to 99",
+      name: "90 to 99",
       array: [80, 99],
     },
     {
       _id: 4,
-      name: "$100 or more",
+      name: "100 or more",
       array: [100, 9999],
     },
   ];
